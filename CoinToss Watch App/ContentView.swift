c4a3f043cc070cb989db61838d2a5130dfc6af21
Mochamad Lucky Pradana <lucky.pradana@wahyoo.com>
2022-11-09 17:43:58 +0700

    feat: add app

--- CoinToss Watch App/ContentView.swift
+++ CoinToss Watch App/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+enum Coin : CaseIterable {
+    case head, tail
+}
+
 struct ContentView: View {
+    
+    @State private var loading = false
+    @State private var result : Coin? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(getLabel())
+                .padding(.bottom, 24.0)
+                .fontWeight(.bold)
+            
+            Button (action: flipCoin) {
+                Text(result != nil ? "Reset" : "Flip")
+            }.disabled(loading)
         }
         .padding()
+    }
+    
+    func flipCoin () -> Void {
+        if (result != nil){
+            result = nil
+        } else {
+            loading = true
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                
+                result = Coin.allCases.randomElement()
+                loading = false
+            }
+        }
+    }
+    
+    func getLabel() -> String {
+        if (loading){
+            return "Flipping...";
+        } else if (result == .head){
+            return "You got Head!"
+        } else if (result == .tail){
+            return "You got Tail!"
+        }
+        
+        return "Flip The Coin";
     }
 }
 

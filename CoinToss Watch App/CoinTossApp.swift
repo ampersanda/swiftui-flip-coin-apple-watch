0d8e65b37352f745c424c4443b49ed78f5aa7a44
Mochamad Lucky Pradana <lucky.pradana@wahyoo.com>
2022-11-09 17:00:22 +0700

    Initial Commit

---
+++ CoinToss Watch App/CoinTossApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTossApp.swift
+//  CoinToss Watch App
+//
+//  Created by Lucky Pradana on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinToss_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
